60ddebc6555d24a91afc833ab841ae3f0d0fad62
vijay8738 <tawdevijay926@gmail.com>
2023-02-07 16:38:35 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/vijay/app/git_demo/App.java
@@ -0,0 +1,17 @@
+package com.vijay.app.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public void addData(String data) {
+
+		System.out.println("the data is" + data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
